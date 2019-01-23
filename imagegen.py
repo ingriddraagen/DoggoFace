0b7d1855bc845b2389588902600af0386addd06c
Jonatan S. Engstad <jonaengs@gmail.com>
2019-01-23 11:23:03 +0100

    clean up script, add gitignore, put doggoparts into their own folder for ez coding

--- imagegen.py
+++ imagegen.py
@@ -1,31 +1,16 @@
-# Combine multiple images into one.
-#
-# To install the Pillow module on Mac OS X:
-#
-# $ xcode-select --install
-# $ brew install libtiff libjpeg webp little-cms2
-# $ pip install Pillow
-#
-
 from __future__ import print_function
 import os
-
+import random
 from PIL import Image
 
-os.listdir()
-files = [
-'Ears/1.png',
-'Faceshapes/1.png',
-  'Eyes/1.png',
-  'Accessories/1.png',
-  'Noses/1.png']
+# Generate background image, choose from a selection of colors (pretty pink, beautiful blue, etc.)
+background = Image.new('RGB', (2048, 2048), color=(252, 156, 246))
 
+for folder in os.listdir('Doggoparts/'):
+    img_folder = 'Doggoparts/' + folder + '/'
+    img = Image.open(
+        img_folder + random.choice(os.listdir(img_folder))
+    )
+    background.paste(img, (0, 0), img)
 
-
-background = Image.open(files[0])
-
-for file in files[1:]:
-    img = Image.open(file)
-    background.paste(img,(0, 0), img)
-
-background.show()
+background.save('image.png')
