eab7db3b4dc3de1e0a6736c8b0526fbaf8bff2d8
ingriddraagen <ingriddraagen@users.noreply.github.com>
2019-02-21 15:54:27 +0100

    i did some stuff. sorry, i don't actually remember what

--- imagegen.py
+++ imagegen.py
@@ -17,35 +17,61 @@ bg_colors = [
     (255, 200, 87),
     (233, 114, 76),
 ]
-background = Image.new('RGB', (2048, 2048), color=(0, 255, 255))
+# Selecting a random texture for the dog-s fur
+texture_choice = Image.open("Doggoparts/00_Texture/" + random.choice(os.listdir("Doggoparts/00_Texture/")))
 
 # Ingrid fucker opp alt
 
-base = Image.new('RGB', (2048, 2048), color=(0, 255, 255))
-ear =  Image.open("Doggoparts/04_Ears/" + random.choice(os.listdir("Doggoparts/04_Ears/")))
-face =  Image.open("Doggoparts/01_Faceshapes/" + random.choice(os.listdir("Doggoparts/01_Faceshapes/")))
-eyes =  Image.open("Doggoparts/03_Eyes/" + random.choice(os.listdir("Doggoparts/03_Eyes/")))
-texture_choice = Image.open("Doggoparts/00_Texture/" + random.choice(os.listdir("Doggoparts/00_Texture/")))
+def insert_layer_to_image(image, layer):
+    image.paste(layer, (0, 0), layer)
+    return
+
+def return_radom_file_from_dir(dir):
+    return (random.choice(os.listdir(dir)))
+
+def open_random_image_file(dir, random):
+    return (Image.open(dir + random))
+
+def make_base():
+    base = Image.new('RGB', (2048, 2048), color=(0, 255, 255))
+    ear =  Image.open("Doggoparts/01_Ears/" + random.choice(os.listdir("Doggoparts/01_Ears/")))
+    face =  Image.open("Doggoparts/02_Faceshapes/" + random.choice(os.listdir("Doggoparts/02_Faceshapes/")))
+    eyes =  Image.open("Doggoparts/03_Eyes/" + random.choice(os.listdir("Doggoparts/03_Eyes/")))
+    insert_layer_to_image(base, texture_choice)
+    insert_layer_to_image(base, ear)
+    insert_layer_to_image(base, eyes)
+    insert_layer_to_image(base, face)
+    base.save('Outputs/base.png')
+
+def fill(layer):
+    if( layer[0] == 'n'):
+        return False
+    return True
+
+def get_mouth_color(base):
+    rgb_im = base.convert('RGB')
+    mouth_color = rgb_im.getpixel((0, 0))
+    return mouth_color
 
 
-base.paste(texture_choice, (0, 0), texture_choice)
-base.paste(ear, (0, 0), ear)
-base.paste(eyes, (0, 0), eyes)
-base.paste(face, (0, 0), face)
-base.save('base.png')
 
-snoot = Image.open('base.png')
-snooty =  Image.open("Doggoparts/02_Accessories/" + random.choice(os.listdir("Doggoparts/02_Accessories/")))
-rgb_im = base.convert('RGB')
-mouth_color = rgb_im.getpixel((0, 0))
-snoot.paste(snooty, (0, 0), snooty)
-ImageDraw.floodfill(snoot, xy=(1000, 1200), value=mouth_color, thresh=10)
+def finish_base():
+    doggoface = Image.open('Outputs/base.png')
+    accessory_filename = random.choice(os.listdir("Doggoparts/04_Accessories/"))
+    accessory =  Image.open("Doggoparts/04_Accessories/" + accessory_filename)
+    insert_layer_to_image(doggoface, accessory)
+    if fill(accessory_filename):
+        ImageDraw.floodfill(doggoface, xy=(1000, 1200), value=mouth_color, thresh=10)
+    nose =  Image.open("Doggoparts/05_Noses/" + random.choice(os.listdir("Doggoparts/05_Noses/")))
+    insert_layer_to_image(doggoface, nose)
+    aggressive_floodfill(doggoface,  xy=(0, 0), value=random.choice(bg_colors))
 
-nose =  Image.open("Doggoparts/05_Noses/" + random.choice(os.listdir("Doggoparts/05_Noses/")))
-snoot.paste(nose, (0, 0), nose)
-aggressive_floodfill(snoot,  xy=(0, 0), value=random.choice(bg_colors))
+    doggoface.save('Outputs/doggo.png')
 
-snoot.save('snootes.png')
+make_base()
+
+finish_base()
+
 
 
 '''
